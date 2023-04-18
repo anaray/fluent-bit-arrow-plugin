c86780d513d5a979e34216f422ebd0347aec1143
Anumodh N <anumodh.n@workday.com>
2023-04-18 12:19:37 -0700

    create a new package pkg

--- main.go
+++ main.go
@@ -8,20 +8,17 @@ import (
 	"github.com/fluent/fluent-bit-go/output"
 )
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
 
+	"github.com/anaray/fluent-bit-arrow-plugin/pkg/plugin"
+
 	arrowschema "github.com/anaray/fluent-bit-arrow-plugin/internal/arrow"
-	"github.com/apache/arrow/go/arrow/memory"
 	"github.com/apache/arrow/go/v12/arrow"
 	"github.com/apache/arrow/go/v12/arrow/array"
-	"github.com/apache/arrow/go/v12/arrow/flight"
-	"github.com/apache/arrow/go/v12/arrow/ipc"
 	"github.com/itchyny/timefmt-go"
-	"google.golang.org/grpc"
 )
 
 const PluginName = "arrow"
@@ -33,96 +30,29 @@ const FlightServerUrl = "Arrow_Flight_Server_Url"
 const InferSchema = "Infer_Schema"
 const SchemaFile = "Schema_File"
 
-type Plugin interface {
-	Create(ctx unsafe.Pointer) (PluginContext, error)
-	WriteString(field string, val string) error
-}
-
-type PluginContext struct {
-	Id          string
-	TimeFields  map[string]string
-	recordCount int
-	Schema      *arrow.Schema
-	builder     *ArrowRecordBuilder
-	flightSvc   *ArrowFlightService
-}
-
-type ArrowRecordBuilder struct {
-	recordBuilder *array.RecordBuilder
-	fieldIndex    map[string]array.Builder
-}
-
-func NewRecordBuilder(schema *arrow.Schema) (*ArrowRecordBuilder, error) {
-	b := array.NewRecordBuilder(&memory.GoAllocator{}, schema)
-
-	bldr := &ArrowRecordBuilder{
-		recordBuilder: b,
-		fieldIndex:    make(map[string]array.Builder, len(schema.Fields())),
-	}
-
-	fields := schema.Fields()
-	for indx, field := range fields {
-		bldr.fieldIndex[field.Name] = b.Field(indx)
-	}
-	return bldr, nil
-}
-
-type ArrowFlightService struct {
-	ArrowFlightServerUrl string
-	Writer               flight.Writer
-}
-
-func NewFlightService(url string, schema *arrow.Schema) (*ArrowFlightService, error) {
-	conn, err := grpc.Dial(url, grpc.WithInsecure()) // TODO: convert this into secure
-	if err != nil {
-		return nil, fmt.Errorf("failed to create grpc connection [%s]", url)
-	}
-	//defer conn.Close()
-
-	client := flight.NewFlightServiceClient(conn)
-	p, err := client.DoPut(context.Background()) // TODO: Pass GRPC call option here
-	if err != nil {
-		return nil, err
-	}
-	wtr := flight.NewRecordWriter(p, ipc.WithSchema(schema))
-	desc := &flight.FlightDescriptor{
-		Type: flight.DescriptorUNKNOWN,
-	}
-	wtr.SetFlightDescriptor(desc)
-
-	return &ArrowFlightService{
-		ArrowFlightServerUrl: url,
-		Writer:               *wtr,
-	}, nil
-}
-
-func (svc ArrowFlightService) Write(record arrow.Record) {
-	log.Printf("Write called &v\n", record)
-	svc.Writer.Write(record)
-}
-
+// FluentArrowPlugin
 type FluentArrowPlugin struct {
-	contexts map[string]*PluginContext
+	contexts map[string]*plugin.PluginContext
 }
 
-func NewPlugin() *FluentArrowPlugin {
-	return &FluentArrowPlugin{
-		contexts: make(map[string]*PluginContext),
+func NewPlugin() plugin.Plugin {
+	return FluentArrowPlugin{
+		contexts: make(map[string]*plugin.PluginContext),
 	}
 }
 
-var plugin = NewPlugin()
+var arrowPlugin plugin.Plugin = NewPlugin()
 
-func (p *FluentArrowPlugin) Create(ctx unsafe.Pointer) (*PluginContext, error) {
+func (p FluentArrowPlugin) Create(ctx unsafe.Pointer) (*plugin.PluginContext, error) {
 	errMsg := "mandatory plugin configuration [%s] missing"
-	c := PluginContext{
+	c := plugin.PluginContext{
 		TimeFields: make(map[string]string),
 	}
 
 	// 1) Id
 	id := output.FLBPluginConfigKey(ctx, Id)
 	if id == "" {
-		return &PluginContext{}, fmt.Errorf(errMsg, Id)
+		return &plugin.PluginContext{}, fmt.Errorf(errMsg, Id)
 	}
 	c.Id = id
 
@@ -146,17 +76,17 @@ func (p *FluentArrowPlugin) Create(ctx unsafe.Pointer) (*PluginContext, error) {
 	// 3) Arrow_Flight_Server_Url
 	fs := output.FLBPluginConfigKey(ctx, FlightServerUrl)
 	if fs == "" {
-		return &PluginContext{}, fmt.Errorf(errMsg, FlightServerUrl)
+		return &plugin.PluginContext{}, fmt.Errorf(errMsg, FlightServerUrl)
 	}
 
 	// 4) Schema_File
 	sf := output.FLBPluginConfigKey(ctx, SchemaFile)
 	if sf == "" {
-		return &PluginContext{}, fmt.Errorf(errMsg, SchemaFile)
+		return &plugin.PluginContext{}, fmt.Errorf(errMsg, SchemaFile)
 	}
 	s, err := parseSchema(sf)
 	if err != nil {
-		return &PluginContext{}, err
+		return &plugin.PluginContext{}, err
 	}
 	c.Schema = s
 
@@ -168,55 +98,52 @@ func (p *FluentArrowPlugin) Create(ctx unsafe.Pointer) (*PluginContext, error) {
 	}
 
 	// Set schema to RecordBuilder
-	b, err := NewRecordBuilder(c.Schema)
+	b, err := plugin.NewRecordBuilder(c.Schema)
 	if err != nil {
-		return &PluginContext{}, err
+		return &plugin.PluginContext{}, err
 	}
-	c.builder = b
+	c.Builder = b
 
 	// create ArrowFlightService
-	fltSvc, err := NewFlightService(fs, s)
+	fltSvc, err := plugin.NewFlightService(fs, s)
 	if err != nil {
-		return &PluginContext{}, err
+		return &plugin.PluginContext{}, err
 	}
-	c.flightSvc = fltSvc
+	c.FlightSvc = fltSvc
 	return &c, nil
 }
 
 func (p FluentArrowPlugin) WriteString(pluginId string, fieldName string, values []string, valid []bool) {
-	log.Print("PluginId :" + pluginId)
-	log.Print("fieldName :" + fieldName)
-	//pick the correct builder and append value to the field
-	p.contexts[pluginId].builder.fieldIndex[fieldName].(*array.StringBuilder).AppendValues(values, valid)
+	p.contexts[pluginId].Builder.FieldIndex[fieldName].(*array.StringBuilder).AppendValues(values, valid)
 }
 
 func (p FluentArrowPlugin) WriteInt64(pluginId string, fieldName string, values []int64, valid []bool) {
 	//pick the correct builder and append value to the field
-	p.contexts[pluginId].builder.fieldIndex[fieldName].(*array.Int64Builder).AppendValues(values, valid)
+	p.contexts[pluginId].Builder.FieldIndex[fieldName].(*array.Int64Builder).AppendValues(values, valid)
 }
 
 func (p FluentArrowPlugin) WriteUInt64(pluginId string, fieldName string, values []uint64, valid []bool) {
 	//pick the correct builder and append value to the field
-	p.contexts[pluginId].builder.fieldIndex[fieldName].(*array.Uint64Builder).AppendValues(values, valid)
+	p.contexts[pluginId].Builder.FieldIndex[fieldName].(*array.Uint64Builder).AppendValues(values, valid)
 }
 
 func (p FluentArrowPlugin) WriteInt32(pluginId string, fieldName string, values []int32, valid []bool) {
 	//pick the correct builder and append value to the field
-	p.contexts[pluginId].builder.fieldIndex[fieldName].(*array.Int32Builder).AppendValues(values, valid)
+	p.contexts[pluginId].Builder.FieldIndex[fieldName].(*array.Int32Builder).AppendValues(values, valid)
 }
 
 func (p FluentArrowPlugin) WriteFloat64(pluginId string, fieldName string, values []float64, valid []bool) {
 	//pick the correct builder and append value to the field
-	p.contexts[pluginId].builder.fieldIndex[fieldName].(*array.Float64Builder).AppendValues(values, valid)
+	p.contexts[pluginId].Builder.FieldIndex[fieldName].(*array.Float64Builder).AppendValues(values, valid)
 }
 
 func (p FluentArrowPlugin) WriteFloat32(pluginId string, fieldName string, values []float32, valid []bool) {
 	//pick the correct builder and append value to the field
-	p.contexts[pluginId].builder.fieldIndex[fieldName].(*array.Float32Builder).AppendValues(values, valid)
+	p.contexts[pluginId].Builder.FieldIndex[fieldName].(*array.Float32Builder).AppendValues(values, valid)
 }
 
 func (p FluentArrowPlugin) WriteTimeStamp(pluginId string, fieldName string, values []arrow.Timestamp, valid []bool) {
-	p.contexts[pluginId].builder.fieldIndex[fieldName].(*array.TimestampBuilder).AppendValues(values, valid)
+	p.contexts[pluginId].Builder.FieldIndex[fieldName].(*array.TimestampBuilder).AppendValues(values, valid)
 }
 
 func parseSchema(file string) (*arrow.Schema, error) {
@@ -249,26 +176,20 @@ func FLBPluginRegister(def unsafe.Pointer) int {
 
 //export FLBPluginInit
 func FLBPluginInit(ctx unsafe.Pointer) int {
-	id := output.FLBPluginConfigKey(ctx, Id)
-	c, err := plugin.Create(ctx)
+	c, err := arrowPlugin.Create(ctx)
 	if err != nil {
 		log.Fatalf("%v\n", err)
 		return FLBPluginExit()
 	}
 
-	plugin.contexts[id] = c
-	output.FLBPluginSetContext(ctx, id)
+	arrowPlugin.(FluentArrowPlugin).contexts[c.Id] = c
+	output.FLBPluginSetContext(ctx, c.Id)
 	return output.FLB_OK
 }
 
 //export FLBPluginFlushCtx
 func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int {
 	id := output.FLBPluginGetContext(ctx).(string)
-	if id == "" {
-		log.Fatalf("failed to read the plugin id")
-		return FLBPluginExit()
-	}
-
 	dec := output.NewDecoder(data, int(length))
 	for {
 		ret, _, record := output.GetRecord(dec)
@@ -276,61 +197,56 @@ func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int 
 			break
 		}
 
-		fmt.Println("----")
 		for k, v := range record {
 			key := k.(string)
-			// proces only if the fields are present in the arrow schema for the plugin
-			if _, ok := plugin.contexts[id].builder.fieldIndex[key]; !ok {
-				continue
-			}
-			fmt.Println("key :" + key)
-
-			switch v := v.(type) {
-			case []uint8:
-				strVal := string(v)
-				if dateFormat, ok := plugin.contexts[id].TimeFields[key]; ok {
-					t, err := timefmt.Parse(strVal, dateFormat)
-					if err != nil {
-						log.Printf("failed to parse date %s using format %s", strVal, dateFormat)
-						//return output.FLB_ERROR
+			// process only if the fields are present in the arrow schema for the plugin
+			if _, ok := arrowPlugin.(FluentArrowPlugin).contexts[id].Builder.FieldIndex[key]; ok {
+				switch v := v.(type) {
+				case []uint8:
+					strVal := string(v)
+					if dateFormat, ok := arrowPlugin.(FluentArrowPlugin).contexts[id].TimeFields[key]; ok {
+						t, err := timefmt.Parse(strVal, dateFormat)
+						if err != nil {
+							log.Printf("failed to parse date %s using format %s", strVal, dateFormat)
+							//return output.FLB_ERROR
+						}
+						int64Val := t.Unix()
+						log.Printf("ctx= %s, got int64 value for key=%s value=%d", id, key, int64Val)
+						arrowPlugin.WriteTimeStamp(id, key, []arrow.Timestamp{arrow.Timestamp(int64Val)}, []bool{true})
+						//z, o := t.Zone()
+					} else {
+						log.Printf("ctx= %s, got string value for key=%s value=%s", id, key, strVal)
+						arrowPlugin.WriteString(id, key, []string{strVal}, []bool{true})
 					}
-					int64Val := t.Unix()
-					log.Printf("ctx= %s, got int64 value for key=%s value=%d", id, key, int64Val)
-					plugin.WriteTimeStamp(id, key, []arrow.Timestamp{arrow.Timestamp(int64Val)}, []bool{true})
-					//z, o := t.Zone()
-				} else {
-					log.Printf("ctx= %s, got string value for key=%s value=%s", id, key, strVal)
-					plugin.WriteString(id, key, []string{strVal}, []bool{true})
+				case float64:
+					flt64Val := v
+					log.Printf("ctx= %s, got float value for key=%s value=%f", id, key, flt64Val)
+					arrowPlugin.WriteFloat64(id, key, []float64{flt64Val}, []bool{true})
+				case int:
+					intVal := v
+					log.Printf("ctx= %s, got int value for key=%s value=%d", id, key, intVal)
+					arrowPlugin.WriteInt64(id, key, []int64{int64(intVal)}, []bool{true})
+				case int64:
+					intVal := v
+					log.Printf("ctx= %s, got int64 value for key=%s value=%d", id, key, intVal)
+					arrowPlugin.WriteInt64(id, key, []int64{intVal}, []bool{true})
+				case nil:
+					strVal := ""
+					log.Printf("ctx= %s, got nil value for key=%s value=%f", id, key, strVal)
+				default:
+					strVal := fmt.Sprintf("%v", v)
+					log.Printf("ctx= %s, got default value for key=%s value=%f", id, key, strVal)
 				}
-			case float64:
-				flt64Val := v
-				log.Printf("ctx= %s, got float value for key=%s value=%f", id, key, flt64Val)
-				plugin.WriteFloat64(id, key, []float64{flt64Val}, []bool{true})
-			case int:
-				intVal := v
-				log.Printf("ctx= %s, got int value for key=%s value=%d", id, key, intVal)
-				plugin.WriteInt64(id, key, []int64{int64(intVal)}, []bool{true})
-			case int64:
-				intVal := v
-				log.Printf("ctx= %s, got int64 value for key=%s value=%d", id, key, intVal)
-				plugin.WriteInt64(id, key, []int64{intVal}, []bool{true})
-			case nil:
-				strVal := ""
-				log.Printf("ctx= %s, got nil value for key=%s value=%f", id, key, strVal)
-			default:
-				strVal := fmt.Sprintf("%v", v)
-				log.Printf("ctx= %s, got default value for key=%s value=%f", id, key, strVal)
 			}
 
 		}
-		plugin.contexts[id].recordCount++
-		if plugin.contexts[id].recordCount > 5 {
+		arrowPlugin.(FluentArrowPlugin).contexts[id].RecordCount++
+		if arrowPlugin.(FluentArrowPlugin).contexts[id].RecordCount > 5 {
 			//call make record and flush
-			r := plugin.contexts[id].builder.recordBuilder.NewRecord()
+			r := arrowPlugin.(FluentArrowPlugin).contexts[id].Builder.RecordBuilder.NewRecord()
 			defer r.Release()
-			fmt.Println("In Flight")
-			plugin.contexts[id].flightSvc.Write(r)
-			plugin.contexts[id].recordCount = 0
+			arrowPlugin.(FluentArrowPlugin).contexts[id].FlightSvc.Write(r)
+			arrowPlugin.(FluentArrowPlugin).contexts[id].RecordCount = 0
 		}
 	}
 	return output.FLB_OK
---
+++ pkg/plugin/plugin.go
@@ -0,0 +1,95 @@
+package plugin
+
+import (
+	"context"
+	"fmt"
+	"unsafe"
+
+	"github.com/apache/arrow/go/arrow/memory"
+	"github.com/apache/arrow/go/v12/arrow"
+	"github.com/apache/arrow/go/v12/arrow/array"
+	"github.com/apache/arrow/go/v12/arrow/flight"
+	"github.com/apache/arrow/go/v12/arrow/ipc"
+	"google.golang.org/grpc"
+)
+
+// Plugin provides an interface for initialising a plugin and to build arrow arrays
+type Plugin interface {
+	Create(ctx unsafe.Pointer) (*PluginContext, error)
+	WriteString(pluginId string, fieldName string, values []string, valid []bool)
+	WriteInt64(pluginId string, fieldName string, values []int64, valid []bool)
+	WriteUInt64(pluginId string, fieldName string, values []uint64, valid []bool)
+	WriteInt32(pluginId string, fieldName string, values []int32, valid []bool)
+	WriteFloat64(pluginId string, fieldName string, values []float64, valid []bool)
+	WriteFloat32(pluginId string, fieldName string, values []float32, valid []bool)
+	WriteTimeStamp(pluginId string, fieldName string, values []arrow.Timestamp, valid []bool)
+}
+
+// PluginContext wraps context required for an individual plugin
+
+// Configuratiions for each plugin is stored in this context
+type PluginContext struct {
+	Id          string
+	TimeFields  map[string]string
+	RecordCount int
+	Schema      *arrow.Schema
+	Builder     *ArrowRecordBuilder
+	FlightSvc   *ArrowFlightService
+}
+
+// ArrowFlightService aids and creates a Arrow Flight Client and Flight Writer.
+type ArrowFlightService struct {
+	ArrowFlightServerUrl string
+	Writer               flight.Writer
+}
+
+func NewFlightService(url string, schema *arrow.Schema) (*ArrowFlightService, error) {
+	conn, err := grpc.Dial(url, grpc.WithInsecure()) // TODO: convert this into secure
+	if err != nil {
+		return nil, fmt.Errorf("failed to create grpc connection [%s]", url)
+	}
+	//defer conn.Close()
+
+	client := flight.NewFlightServiceClient(conn)
+	p, err := client.DoPut(context.Background()) // TODO: Pass GRPC call option here
+	if err != nil {
+		return nil, err
+	}
+	wtr := flight.NewRecordWriter(p, ipc.WithSchema(schema))
+	desc := &flight.FlightDescriptor{
+		Type: flight.DescriptorUNKNOWN,
+	}
+	wtr.SetFlightDescriptor(desc)
+
+	return &ArrowFlightService{
+		ArrowFlightServerUrl: url,
+		Writer:               *wtr,
+	}, nil
+}
+
+// Write a Record
+func (svc ArrowFlightService) Write(record arrow.Record) {
+	svc.Writer.Write(record)
+}
+
+// ArrowRecordBuilder maps fieldName to its array.Builder
+// This is created based on the given arrow schema.
+type ArrowRecordBuilder struct {
+	RecordBuilder *array.RecordBuilder
+	FieldIndex    map[string]array.Builder
+}
+
+// NewRecordBuilder creates ArrowRecordBuilder, which maps fieldName to
+// array.Builder. This id derived from a configured arrow Schema.
+func NewRecordBuilder(schema *arrow.Schema) (*ArrowRecordBuilder, error) {
+	b := array.NewRecordBuilder(&memory.GoAllocator{}, schema)
+	bldr := &ArrowRecordBuilder{
+		RecordBuilder: b,
+		FieldIndex:    make(map[string]array.Builder, len(schema.Fields())),
+	}
+	fields := schema.Fields()
+	for indx, field := range fields {
+		bldr.FieldIndex[field.Name] = b.Field(indx)
+	}
+	return bldr, nil
+}
